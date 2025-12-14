bd3314e2ece2abc8b40e231c926d00dfccfa157a
cttlo <charlotte.alarie@gmail.com>
2025-12-14 00:20:06 -0500

    Update display-script-2.js

--- microwave_update_euclid/display-script-2.js
+++ microwave_update_euclid/display-script-2.js
@@ -1,16 +1,4 @@
-    const map = L.map('map').setView([45.5048, -73.5769], 16);
-
-     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap &copy; CARTO',
-      maxZoom: 19
-    }).addTo(map);
-
-      const bounds = L.latLngBounds(
-        [45.4985, -73.5865],
-        [45.5105, -73.5655]
-      );
-
-    const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYO34fzZ7CHjhWvLbIf0040V0uHVm0jTYjit8QrFDUmrFBg643QhaL5aeL4YrvweXoPlEUH4IPQS1/pub?gid=0&single=true&output=csv";
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRaYO34fzZ7CHjhWvLbIf0040V0uHVm0jTYjit8QrFDUmrFBg643QhaL5aeL4YrvweXoPlEUH4IPQS1/pub?gid=0&single=true&output=csv";
 
     const group = L.featureGroup().addTo(map);
 
@@ -98,19 +86,3 @@
 
     style: { color: "#0000ff", weight: 2, opacity: 0.9, dashArray: "1, 5" }
   }).addTo(map);
-
-    const panel = document.getElementById("info-panel");
-    const toggleBtn = document.getElementById("toggle-info");
-
-    toggleBtn.addEventListener("click", () => {
-      panel.classList.toggle("collapsed");
-      toggleBtn.textContent = panel.classList.contains("collapsed") ? ">" : "<";;
-    });
-
-    document.getElementById("nav-submit").addEventListener("click", () => {
-      window.location.href = "submit-2.html";
-    });
-
-    document.getElementById("nav-display").addEventListener("click", () => {
-      window.location.href = "display-2.html";
-    });
